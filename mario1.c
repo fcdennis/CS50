03f81f3d0a17ee3f3bdb301cda4ea33f7ce37b4e
Dennis Costa <56238317+fcdennis@users.noreply.github.com>
2020-06-08 11:31:10 -0300

    Update mario1.c
    Final result post and corrected

--- mario1.c
+++ mario1.c
@@ -3,14 +3,25 @@
 
 int main(void)
 {
-    int linha = get_int("Height? ");
-    for(int i = 0; i < linha; i++)
+    int h;
+    do
     {
-        for(int j = 0; j <= i; j++)
+        h = get_int("Height: ");
+    }
+    while(h <1 || h >8);
+    
+    int w = h - 1;
+    for(int i = 0; i <h; i++)
+    {
+        for(int j =w; j >0; j--)
+        {
+            printf(" ");
+        }
+        w--;
+        for(int l =0; l <=i; l++)
         {
             printf("#");
         }
         printf("\n");
     }
-    
 }
