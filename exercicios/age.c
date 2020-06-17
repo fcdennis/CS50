3abe6aa36e1ff5aa612a4db0562a29e48610e29b
Dennis Costa <56238317+fcdennis@users.noreply.github.com>
2020-06-17 19:45:33 -0300

    ola_mund

--- exercicios/age.c
+++ exercicios/age.c
@@ -3,7 +3,10 @@
 
 int main(void)
 {
-    int age = get_int("What's your age?\n");
+    int age;
+    printf("Qual é a sua idade: ");
+    scanf("%i", &age);
     int day = age * 365;
     printf("You have at least %i days of life!\n", day);
+    return 0;
 }
---
+++ exercicios/ola_mundo.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //biblioteca necessária para entrada e saída de dados
+
+int main(void)//função principal
+{//início da função
+	printf("Olá, mundo!");
+	return 0;//valor de retorno da função main(significa que rodou o programa com sucesso.
+}//fim da função
