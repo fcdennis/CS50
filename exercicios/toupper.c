d10a2b175852bf733b7c2047aa209b4103851ab0
Dennis Costa <56238317+fcdennis@users.noreply.github.com>
2020-07-14 18:18:36 -0300

    Create toupper.c

---
+++ exercicios/toupper.c
@@ -0,0 +1,29 @@
+//Capitalizes a copy of a string
+
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    //Get a string
+    char *l = get_string("l: ");
+    
+    //Allocate memory for another string
+    char *t = malloc(strlen(l) + 1);
+    
+    //Copy string into memory
+    for (int i = 0, n = strlen(l); i <= n; i++)
+    {
+        t[i] = l[i];
+    }
+    
+    //Capitalize copy
+    t[0] = toupper(t[0]);
+    
+    //Print strings
+    printf("l: %s\n", l);
+    printf("u: %s\n", t);
+}
