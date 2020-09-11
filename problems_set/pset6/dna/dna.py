b5379372ea6655da93a9bda579851bfa1fb36ca9
Dennis Costa <56238317+fcdennis@users.noreply.github.com>
2020-09-11 18:13:11 -0300

    dna final

--- problems_set/pset6/dna/dna.py
+++ problems_set/pset6/dna/dna.py
@@ -1,25 +1,45 @@
+from sys import argv, exit
 import csv
 
-person = {
-    "AGATC": '4',
-    "AATG": '1',
-    "TATC": '5'
-}
+def main():
+    if len(argv) != 3:
+        print('Usage: python dna.py data.csv sequence.txt')
+        exit(1)
 
-with open('databases/small.csv') as csvfile:
-    sampleData = csv.DictReader(csvfile)
-    for row in sampleData:
-        if(row["AGATC"] == person["AGATC"] and row["AATG"] == person["AATG"] and row["TATC"] == person["TATC"]):
-            print(row['name'])
+    csv_path = argv[1]
+    with open(csv_path) as csv_file:
+        database = csv.reader(csv_file)
+        all_sequences = next(database)[1:]
 
-csvfile.close()
+        txt_path = argv[2]
+        with open(txt_path) as txt_file:
+            complete_dna_sequence = txt_file.read()
+            actual = [nitrogen_sequence(complete_dna_sequence, seq) for seq in all_sequences]
 
-count = 0
-sequence = open('sequences/1.txt')
-for c in sequence:
-    for e in c:
-        if(e == 'AATG'):
-            count += 1
+        found_sequence_match(database, actual)
 
-print(count)
-sequence.close()
+
+def nitrogen_sequence(complete_dna_sequence, nitrogen):
+    ans = [0] * len(complete_dna_sequence)
+    for i in range((len(complete_dna_sequence) - len(nitrogen)), -1, -1):
+        if complete_dna_sequence[i: i + len(nitrogen)] == nitrogen:
+            if i + len(nitrogen) > len(complete_dna_sequence) - 1:
+                ans[i] = 1
+            else:
+                ans[i] = 1 + ans[i + len(nitrogen)]
+    
+    return max(ans)
+
+
+def found_sequence_match(reader, actual):
+    for line in reader:
+        person = line[0]
+        values = [int(val) for val in line[1:]]
+        if values == actual:
+            print(person)
+            return
+    print('No match')
+
+
+if __name__ == "__main__":
+    main()
